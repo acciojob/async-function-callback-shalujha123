8f17b0142f97d098cfed08b589ebc3e9c4ee5598
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-02 21:22:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+let output = document.getElementById("output")
+document.getElementById("btn").addEventListener("click",async() => {
+	try{
+		let response = await fetch(url)
+		let data = await response.json()
+		output.innerHTML = `<p>${data.title}</p>`
+	}catch(err){
+		output.innerHTML = "Failed to fetch error"
+	}
+})
